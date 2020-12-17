09c4279f7f6c25a2620cfd734c35acc896404d33
rowenarono95 <rowenarono@gmail.com>
2020-12-18 00:25:58 +0300

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CampsComponent } from './components/camps/camps.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CampsComponent,
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
