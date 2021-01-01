60a4e204359fa39f608a69890385a65f6c87b3a8
Nyash-Mauro <mauricee423@gmail.com>
2021-01-02 00:14:32 +0300

    modified admin and user classes

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,2 +1,9 @@
 export class User {
+    email: string;
+    first_name:string;
+    last_name:string;
+    date_joined:Date;
+    is_admin:boolean;
+    is_staff:boolean;
+    is_active:boolean;
 }
