c36258ea60421541b9a72eaee959739112e5b4cf
Nyash-Mauro <mauricee423@gmail.com>
2021-01-02 00:28:19 +0300

    created class and details of class profile,donations and conditions

--- src/app/models/conditions.ts
+++ src/app/models/conditions.ts
@@ -1,3 +1,5 @@
 export class Conditions {
-    
+    condition_name:string;
+    description:string;
+    other_details:string;
 }
--- src/app/models/donations.ts
+++ src/app/models/donations.ts
@@ -1,2 +1,10 @@
+import { Identifiers } from "@angular/compiler";
 export class Donations {
+    User:Identifiers;
+    facility:string;
+    donate_date:Date;
+    last_donate_date:Date;
+    location:string;
+    blood_group:string;
+    medical_condition:string;
 }
