d8b577bbeedbcf9d373a2e0e65a51cca08ba9f50
Nyash-Mauro <mauricee423@gmail.com>
2021-01-02 17:25:52 +0300

    added some admin-pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-// import { ProfileComponent } from './components/profile/profile.component';
-// import { LoginComponent } from './login/login.component';
-// import { LogoutComponent } from './logout/logout.component';
-// import { RegisterComponent } from './register/register.component';
-// import { AuthGuard } from './guards/auth-guard.service';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
 
   {path: '', redirectTo: 'login', pathMatch:'full'},
   {path: 'home', component: HomeComponent,},
-  // { path: 'register', component: RegisterComponent},
 
-  // {path: 'profile', component: ProfileComponent},
+  {path: 'profile', component: ProfileComponent},
+  { path: 'signup', component: SignupComponent },
 
-
-  // { path: 'login', component: LoginComponent },
-  // { path: 'logout', component: LogoutComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,14 @@ import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
-import { AdminPagesComponent } from './components/admin-pages/admin-pages.component';
+import { AdminPagesComponent } from './components/admin-dashboard/admin-pages/admin-pages.component';
 import { RecipientFormComponent } from './components/recipient-form/recipient-form.component';
-import { AdminRecordsComponent } from './components/admin-records/admin-records.component';
+import { AdminRecordsComponent } from './components/admin-dashboard/admin-records/admin-records.component';
 import { ProfileComponent } from './components/profile/profile.component';
-// import { SignupComponent } from './signup/signup.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { AdminAddUserComponent } from './components/admin-dashboard/admin-add-user/admin-add-user.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +28,10 @@ import { ProfileComponent } from './components/profile/profile.component';
     RecipientFormComponent,
     AdminRecordsComponent,
     ProfileComponent,
-    // SignupComponent
+    SignupComponent,
+    LoginComponent,
+    LogoutComponent,
+    AdminAddUserComponent
   ],
   imports: [
     BrowserModule,
