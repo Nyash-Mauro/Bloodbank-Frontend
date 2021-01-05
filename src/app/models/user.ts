03bf5240ae873cc584f5f51b50edf02bfa45101a
Nyash-Mauro <mauricee423@gmail.com>
2021-01-05 15:11:19 +0300

    changed x

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,9 +1,12 @@
 export class User {
-    email: string;
-    first_name:string;
-    last_name:string;
-    date_joined:Date;
-    is_admin:boolean;
-    is_staff:boolean;
-    is_active:boolean;
+    constructor(
+        public email: string,
+        public first_name:string,
+        public last_name:string,
+        date_joined:Date,
+        is_admin:boolean,
+        is_staff:boolean,
+        is_active:boolean,
+    ){ }
+
 }
