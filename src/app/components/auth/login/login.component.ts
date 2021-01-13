e49d2d6474023e8266bee9d48602159458936c4a
rowenarono95 <rowenarono@gmail.com>
2021-01-13 17:14:06 +0300

    add donor form

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -13,25 +13,40 @@ export class LoginComponent implements OnInit {
 
   constructor(public authService: AuthService, private router: Router) { }
 
-  role: String;
-  email: String;
-  password: String;
+  // role: String;
+  // email: String;
+  // password: String;
   ngOnInit(): void {
   }
-  login() {
-    var userinfo = new Userinfo(this.email, this.password)
-    this.authService.login(userinfo).subscribe(res => {
+  // login() {
+  //   var userinfo = new Userinfo(this.email, this.password)
+  //   this.authService.login(userinfo).subscribe(res => {
+  //     if (res['access']) {
+  //       sessionStorage.setItem('token', res['access'])
+  //       if (this.role == "1") {
+  //         console.log('donor');
+  //         //redirect to donor dashbord
+  //       } else {
+  //         console.log('recipient');
+
+  //         //redirect to recipient dashboard
+  //       }
+  //       // this.router.navigate(['home'])
+  //     }
+  //   })
+  // }
+
+  login(){
+    console.log(this.authService.login_form.value);
+    
+    this.authService.login(this.authService.login_form.value).subscribe(res=>{
       if (res['access']) {
         sessionStorage.setItem('token', res['access'])
-        if (this.role == "1") {
-          console.log('donor');
-          //redirect to donor dashbord
-        } else {
-          console.log('recipient');
-
-          //redirect to recipient dashboard
-        }
-        // this.router.navigate(['home'])
+        alert("Logged in successfully")
+        this.router.navigate(['donor'])
+      }
+      else{
+        alert("invalid credentials")
       }
     })
   }
